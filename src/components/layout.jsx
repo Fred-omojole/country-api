3bf8043bc2c6e09daf0a311fac4e45d65204684a
Fred-Omojole Omoyele <omoyele.fred-omojole@stu.cu.edu.ng>
2024-03-12 14:43:50 +0100

    changes

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
-
+// import { SpeedInsights } from "@vercel/speed-insights/react"
 // import Header from "./header";
 import Field from "./hero";
 
 const Layout = () => {
   return (
     <>
+    {/* <SpeedInsights/> */}
       <Field />
       <Outlet />
     </>
