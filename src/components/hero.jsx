2a8ee814c565601b7ecd5ca9d82a03b25e882e57
Fred-Omojole Omoyele <omoyele.fred-omojole@stu.cu.edu.ng>
2024-03-18 14:22:05 +0100

    changes

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -10,19 +10,19 @@ import {
 
 const Field = () => {
   return (
-    <section className=" p-5 lg:flex lg:items-center lg:justify-between bg-slate-200 dark:bg-slate-700">
-      <div>
+    <section className=" w-full overflow-hidden py-4 lg:flex lg:items-center lg:justify-between bg-slate-200 dark:bg-slate-700">
+      <div className=" px-4 md:px-0 ">
         <Input
           placeholder=" Search for country..."
-          className=" w-[400px]  p-5 m-4 mb-10 mx-auto bg-[#d1d5db] dark:bg-slate-600 dark:text-white  text-black font-semibold border-none hover:bg-gray-700"
+          className=" text-start w-[350px] md:w-[400px] p-5 m-4 mb-10 mx-auto bg-[#d1d5db] dark:bg-slate-600 overflow-hidden dark:text-white  text-black font-semibold border-none hover:bg-gray-700"
           prefix={<SearchIcon className="text-black dark:text-white mx-2" />}
         />
       </div>
-      <div className="mb-6 dark:text-[#d1d5db]">
-        <Select className="">
+      <div className="mb-6 dark:text-[#d1d5db] px-8 ">
+        <Select className=" ">
           <SelectTrigger className="w-[300px] bg-white dark:bg-slate-600 border-none ">
             <SelectValue
-              className="dark:text-white"
+              className="dark:text-white  "
               placeholder="Filter by Region"
             />
           </SelectTrigger>
